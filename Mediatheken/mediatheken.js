d82b505c049fe3a34759e3d47233480c004ace19
druelie <druelie@hotmail.com>
2014-12-07 15:11:10 +0100

    asd

--- Mediatheken/mediatheken.js
+++ Mediatheken/mediatheken.js
@@ -32,20 +32,92 @@
 	page.contents = 'items';
 	page.metadata.logo = logo;
 	page.metadata.title = pInfo.title;
-        page.loading = true;
-	var doc = showtime.httpReq(BASE_URL).toString().match(/\.Channels = ([\S\s]*?);/)[1];
+        page.appendItem(PREFIX + ':ard', 'station', {
+	    station: 'ARD',
+	    title: 'ARD',
+	    description: 'Mediathek der ARD',
+	    icon: plugin.path + 'img/ARD.png'
+	});
+        page.appendItem(PREFIX + ':zdf', 'directory', {
+	    station: 'ZDF',
+	    title: 'ZDF',
+	    description: 'Mediathek des ZDF',
+	    icon: plugin.path + 'img/ZDF.png'
+	});
+        page.appendItem(PREFIX + ':ndr', 'directory', {
+	    station: 'NDR',
+	    title: 'NDR',
+	    description: 'Mediathek des NDR',
+	    icon: plugin.path + 'img/NDR.png'
+	});
+        page.appendItem(PREFIX + ':arte', 'directory', {
+	    station: 'arte +7',
+	    title: 'arte +7',
+	    description: 'Mediathek von arte',
+	    icon: plugin.path + 'img/arte7.png'
+	});
+        page.appendItem(PREFIX + ':3sat', 'directory', {
+	    station: '3sat',
+	    title: '3sat',
+	    description: 'Mediathek von 3sat',
+	    icon: plugin.path + 'img/3sat.png'
+	});
+        page.appendItem(PREFIX + ':rtl', 'directory', {
+	    station: 'RTL now',
+	    title: 'RTL now',
+	    description: 'Mediathek von RTL',
+	    icon: plugin.path + 'img/RTLnow_.png'
+	});
+        page.appendItem(PREFIX + ':sat1', 'directory', {
+	    station: 'Sat.1',
+	    title: 'Sat.1',
+	    description: 'Mediathek von Sat.1',
+	    icon: plugin.path + 'img/Sat.1.png'
+	});
+        page.appendItem(PREFIX + ':vox', 'directory', {
+	    station: 'VOX now',
+	    title: 'VOX now',
+	    description: 'Mediathek von VOX',
+	    icon: plugin.path + 'img/VOXnow.png'
+	});
+        page.appendItem(PREFIX + ':kabel1', 'directory', {
+	    station: 'Kabel 1',
+	    title: 'Kabel 1',
+	    description: 'Mediathek von Kabel 1',
+	    icon: plugin.path + 'img/Kabel_eins.png'
+	});
         page.loading = false;
+    });
+
+    // arte page
+    plugin.addURI(PREFIX + ':arte', function(page) {
+        var BASE_URL = 'http://www.arte.tv';
+	page.type = 'directory';
+	page.metadata.glwview = plugin.path + 'views/array.view';
+	page.contents = 'items';
+	page.metadata.logo = logo;
+	page.metadata.title = pInfo.title;
+        page.loading = true;
+	var doc = showtime.httpReq(BASE_URL+'/guide/de/plus7.json').toString();
         var json = showtime.JSONDecode(doc);
-        for (var i in json) {
-            var icon = json[i].images.default.match(/(^[^\{]*)/)[1];
-	    page.appendItem('icecast:http://listen.di.fm/public3/'+ json[i].key + '.pls', 'station', {
-		station: json[i].name,
-		title: json[i].name,
-		description: json[i].description_short,
-		icon: icon.substr(0, 4) == 'http' ? icon : 'http:' + icon + '?size=150x150',
-		album_art: icon.substr(0, 4) == 'http' ? icon : 'http:' + icon + '?size=150x150',
-		album: ''
+
+        for (var i in json.videos) {
+            var arte_vp_url = 'http://arte.tv/papi/tvguide/videos/stream/player/D/' + json.videos[i].em + '_PLUS7-D/ALL/ALL.json';
+            var vp = showtime.JSONDecode(showtime.httpReq(arte_vp_url).toString());
+
+ 	    page.appendItem(vp.videoJsonPlayer.VSR.HTTP_MP4_SQ_1.url, 'video', {
+		station:     json.videos[i].title,
+		title:       json.videos[i].title,
+		description: vp.videoJsonPlayer.VDE + '\n\nVerfügbar bis: ' + json.videos[i].airdate_long,
+		icon:        json.videos[i].image_url,
+		album_art:   json.videos[i].image_url,
+		album:       json.videos[i].video_channels,
+		duration:    vp.videoJsonPlayer.VDU,
+		genre:       vp.videoJsonPlayer.genre,
+                views:       vp.videoJsonPlayer.VVI,
+		rating:      vp.videoJsonPlayer.videoRank * 10
 	    });
 	};
+        page.loading = false;
     });
 })(this);
